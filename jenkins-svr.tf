942efce52a3ca55cd6d86f4f8d4b2c97a309c05b
elaw2k4 <elaw2k2@gmail.com>
2023-08-16 20:04:39 -0400

    Create jenkins-svr.tf

---
+++ jenkins-svr.tf
@@ -0,0 +1,27 @@
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "jenkins-server" {
+  ami                         = data.aws_ami.latest-amazon-linux-image.id
+  instance_type               = var.instance_type
+  key_name                    = var.instance_keypair
+  subnet_id                   = aws_subnet.demo-public-subnet-1.id
+  vpc_security_group_ids      = [aws_default_security_group.default-sg.id]
+  availability_zone           = var.availability_zone
+  associate_public_ip_address = true
+  user_data                   = file("install-jenkins.sh")
+  tags = {
+    Name = "walejenkins-server"
+  }
+
+}
