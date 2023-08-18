2836bdee972ec30b5080ab64578dcefc2d10032e
elaw2k4 <elaw2k2@gmail.com>
2023-08-18 18:24:35 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -54,6 +54,18 @@ resource "aws_default_security_group" "default-sg" {
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 9000
+    to_port     = 9092
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
   egress {
     from_port   = 0
     to_port     = 0
